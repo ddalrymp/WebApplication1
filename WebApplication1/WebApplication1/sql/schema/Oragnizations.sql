d2c0ff2c5d199b3dbe0448891e1d3b89632f5d94
Derek Dalrymple <derek.k.dalrymple@gmail.com>
2016-03-07 13:53:37 -0600

    Added Organizations.sql

---
+++ WebApplication1/WebApplication1/sql/schema/Oragnizations.sql
@@ -0,0 +1,14 @@
+﻿SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[Organizations](
+       [Guid] [uniqueidentifier] NOT NULL,
+       [Name] [nvarchar(50)] NOT NULL,
+       [Created] [datetime] NOT NULL CONSTRAINT [DF_WebCart_Created]  DEFAULT (getdate()),
+       [Modified] [datetime] NOT NULL CONSTRAINT [DF_WebCart_Modified]  DEFAULT (getdate()),
+       [Finished] [datetime] NULL
+) ON [PRIMARY]
+
+GO
+SET ANSI_PADDING OFF
