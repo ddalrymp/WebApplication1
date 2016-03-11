c49acdfe53d714d683bff17aef7a60efa52a422d
Derek Dalrymple <derek.k.dalrymple@gmail.com>
2016-03-11 16:04:43 -0600

    Build on laptop command.

---
+++ WebApplication1/build-on-laptop.cmd
@@ -0,0 +1,1 @@
+C:\windows\Microsoft.NET\Framework64\v4.0.30319\MSBuild.exe C:\Users\ddalrymp.ORADEV\Source\Repos\WebApplication1\WebApplication1\build.xml /p:VisualStudioVersion=14.0 /p:Configuration=Debug;SolutionName=WebApplication1;ProjectName=WebApplication1
