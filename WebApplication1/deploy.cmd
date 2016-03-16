35bd8ef9b6052b80aeb54b5d7b69b398a0574590
Derek Dalrymple <derek.k.dalrymple@gmail.com>
2016-03-16 15:51:11 -0500

    Generic Deploy

---
+++ WebApplication1/deploy.cmd
@@ -0,0 +1,3 @@
+REM C:\Users\ddalrymp.ORADEV\Source\Repos\WebApplication1\WebApplication1\WebApplication1\obj\Debug\Package\WebApplication1.deploy.cmd /Y
+REM C:\Users\ddalrymp.ORADEV\Source\Repos\WebApplication1\WebApplication1\WebApplication1\obj\Release\Package\WebApplication1.deploy.cmd /Y
+%~dp0\WebApplication1\obj\Release\Package\WebApplication1.deploy.cmd /Y
